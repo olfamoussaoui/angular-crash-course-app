69a468fdb74ddaa240a375971e86857bfd482ba8
olfamoussaoui <moussaoui.olfa1@gmail.com>
2023-11-12 02:21:51 +0100

    add init method in form service

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -4,8 +4,27 @@ import { Injectable } from '@angular/core';
 export class FormService {
   messages: Array<any> = [];
 
-  constructor() {}
+  constructor() {
+    this.init();
+  }
 
+  init(): void {
+    this.insert({
+      name: 'Olfa',
+      email: 'moussaoui@mail.com',
+      message: 'Hello world',
+    });
+    this.insert({
+      name: 'Ilyes',
+      email: 'ilyes@mail.com',
+      message: 'Hello world - Ilyes',
+    });
+    this.insert({
+      name: 'Dou',
+      email: 'dou@mail.com',
+      message: 'Hello world - Dou',
+    });
+  }
   insert(message: { name: string; email: string; message: string }): void {
     this.messages.push(message);
   }
